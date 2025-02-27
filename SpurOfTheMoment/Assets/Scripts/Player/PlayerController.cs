9d596a6b818fa0633b9af2a49bc776491529d0ea
Grizzly47 <kpgbc123@gmail.com>
2025-02-27 13:02:55 -0800

    Bah

--- SpurOfTheMoment/Assets/Scripts/Player/PlayerController.cs
+++ SpurOfTheMoment/Assets/Scripts/Player/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private PlayerShoot playerShoot;
     private Animator playerAnimator;
 
-    private enum FacingDirection { Up, Left, Right }
+    private enum FacingDirection { Up, Left, Right, Down }
     private FacingDirection facingDirection = FacingDirection.Up;
 
     private void Awake()
@@ -44,6 +44,7 @@ public class PlayerController : MonoBehaviour
         fire = playerIndex == 0 ? playerMap.Attack_P1 : playerMap.Attack_P2;
         reload = playerIndex == 0 ? playerMap.Reload_P1 : playerMap.Reload_P2;
         dodge = playerIndex == 0 ? playerMap.Dodge_P1 : playerMap.Dodge_P2;
+        facingDirection = playerIndex == 0 ? FacingDirection.Up : FacingDirection.Down;
 
         aim.Enable();
         fire.Enable();
@@ -71,29 +72,37 @@ public class PlayerController : MonoBehaviour
     {
         aimDirection = aim.ReadValue<Vector2>();
 
+        playerAnimator.SetInteger("Direction", (int)facingDirection);
+        playerTransform.localScale = new Vector3(facingDirection == FacingDirection.Left ? -1 : 1, 1, 1);
+
         // Optimized direction setting
         facingDirection = aimDirection switch
         {
-            { y: > 0f } => FacingDirection.Up,
+            { y: > 0f } when playerIndex == 0 => FacingDirection.Up,
+            { y: < 0f } when playerIndex == 1 => FacingDirection.Down,
             { x: > 0f } => FacingDirection.Right,
             { x: < 0f } => FacingDirection.Left,
             _ => facingDirection
         };
 
-        playerAnimator.SetInteger("Direction", (int)facingDirection);
-        playerTransform.localScale = new Vector3(facingDirection == FacingDirection.Left ? -1 : 1, 1, 1);
-
         // Optimized fire point positioning
         Vector3 firePointPos = facingDirection switch
         {
             FacingDirection.Up => new Vector3(0, 1, 0),
+            FacingDirection.Down => new Vector3(0, -1, 0),
             FacingDirection.Right => new Vector3(0.86f, 0.13f, 0f),
-            FacingDirection.Left => new Vector3(-0.86f, 0.13f, 0f),
+            FacingDirection.Left => new Vector3(0.86f, 0.13f, 0f),
             _ => playerShoot.firePoint.localPosition
         };
-        Quaternion firePointRot = facingDirection == FacingDirection.Up
-            ? Quaternion.identity
-            : Quaternion.Euler(0, 0, -75);
+
+        Quaternion firePointRot = facingDirection switch
+        {
+            FacingDirection.Up => Quaternion.identity,
+            FacingDirection.Down => Quaternion.Euler(0, 0, 180),
+            FacingDirection.Left => playerIndex == 0 ? Quaternion.Euler(0, 0, -75) : Quaternion.Euler(0, 0, 255),
+            FacingDirection.Right => playerIndex == 0 ? Quaternion.Euler(0, 0, -75) : Quaternion.Euler(0, 0, 255),
+            _ => Quaternion.identity
+        };
 
         playerShoot.firePoint.localPosition = firePointPos;
         playerShoot.firePoint.localRotation = firePointRot;
